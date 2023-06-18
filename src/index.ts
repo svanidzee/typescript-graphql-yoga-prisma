614efc75a446c7f42e35582cd73858879ecc29be
Irakli Svanidze <svanidzeee@gmail.com>
2023-06-18 18:19:23 +0300

    fix render.com node version problem

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import { createYoga } from 'graphql-yoga';
 import { createServer } from 'node:http';
-
 import { schema } from './schema';
+
+const yoga = createYoga({
+  schema: schema,
+});
+
+const server = createServer(yoga);
 
 const port = Number(process.env.API_PORT) || 4000;
 
-function main() {
-  const yoga = createYoga({ schema });
-  const server = createServer(yoga);
-  server.listen(port, () => {
-    console.info('Server is running on http://localhost:4000/graphql');
-  });
-}
-
-main();
+server.listen(port, () => {
+  console.info('Server is running on http://localhost:4000/graphql');
+});
